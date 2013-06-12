526dcfb4066a757fcdc565b5b70e86f68041c466
Tommy Bishop <bishop.thomas@gmail.com>
2013-06-11 18:48:36 -0700

    refactor

--- cookbooks/test/recipes/encrypted.rb
+++ cookbooks/test/recipes/encrypted.rb
@@ -1,3 +1,8 @@
 dbag = Chef::EncryptedDataBagItem.load "ENCRYPTED_DATABAG", "DATABAGITEM"
 
+Chef::Log.info "Inspect: #{dbag.inspect}"
+Chef::Log.info ''
+Chef::Log.info ''
 Chef::Log.info "foo key == #{dbag['foo']}"
+Chef::Log.info ''
+Chef::Log.info ''
--- test_chef_data_bags.sh
+++ test_chef_data_bags.sh
@@ -15,30 +15,45 @@ case "$1" in
 esac
 
 BASE_RPM_URL=https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64
+KNIFE_CMD="/opt/chef/bin/knife solo data bag create"
+
 rpm -ivh $BASE_RPM_URL/chef-$CHEF_VERSION.el6.x86_64.rpm
 
 /opt/chef/embedded/bin/gem install knife-solo_data_bag --no-rdoc --no-ri
 
 cd /vagrant
 
-rm -f data_bags/DATABAG/DATABAGITEM.json
+rm -rf data_bags
+mkdir data_bags
 
 if [ "$ENCRYPT_DATA_BAGS" == "yes" ]; then
-  echo "Going to encrypt the data bag"
   CHEF_RECIPE="test::encrypted"
-  DATA_BAG_ENCRYPT_OPTION="-s foo"
+  SECRET=foo
+
   mkdir /etc/chef
-  echo foo > /etc/chef/encrypted_data_bag_secret
+  echo $SECRET > /etc/chef/encrypted_data_bag_secret
+
+  $KNIFE_CMD ENCRYPTED_DATABAG DATABAGITEM \
+    --json '{ "id": "DATABAGITEM", "foo": "my_secret" }' \
+    --data-bag-path data_bags/ -s $SECRET
+
+  echo ""
+  echo "Data bag contents:"
+  cat data_bags/ENCRYPTED_DATABAG/DATABAGITEM.json
+  echo ""
+  echo ""
 else
   CHEF_RECIPE="test"
-  DATA_BAG_ENCRYPT_OPTION=""
+
+  $KNIFE_CMD DATABAG DATABAGITEM \
+    --json '{ "id": "DATABAGITEM", "foo": "bar" }' \
+    --data-bag-path data_bags/ $DATA_BAG_ENCRYPT_OPTION
+
+  echo ""
+  echo "Data bag contents:"
+  cat data_bags/DATABAG/DATABAGITEM.json
+  echo ""
+  echo ""
 fi
-
-/opt/chef/bin/knife solo data bag create DATABAG DATABAGITEM \
-  --json '{ "id": "DATABAGITEM", "foo": "bar" }' \
-  --data-bag-path data_bags/ $DATA_BAG_ENCRYPT_OPTION
-
-echo "Data bag contents:"
-cat data_bags/DATABAG/DATABAGITEM.json
 
 chef-solo -o "recipe[$CHEF_RECIPE]" -c config.rb -l info
